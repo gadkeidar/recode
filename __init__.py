12d9e51ce027ff117987ce377f427caaf30c721e
gadkeidar <gadkeidar@gmail.com>
2020-10-14 10:25:19 -0400

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "1.0.0"
+__author__ = "Gad Keidar"
+__author_email__ = "gadkeidar@gmail.com"
+__description__ = """Converts ICD-10 codes to CDC/NCHS Selected Causes of Death codes."""
