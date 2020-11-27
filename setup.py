799214cebdfd43fb1b99321267a241855ac25033
gadkeidar <gadkeidar@gmail.com>
2020-11-27 12:23:35 -0500

    Rename recode to icd10-c2cdc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='recode',
+    name='icd10-c2cdc',
     version='0.0.1',
     author='Gad Keidar',
     author_email='gadkeidar@gmail.com',
